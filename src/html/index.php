515852822016fd0949533e1ba9c708933a14a7fa
Benjamin Bykowski <benjamin.bykowski@gmail.com>
2018-07-13 07:16:09 -0500

    Tweaks to basic layout example

--- src/html/index.php
+++ src/html/index.php
@@ -12,9 +12,8 @@
     <div class="container main">
     	<h1>Ohai World!</h1>
     	<p>I can haz templait?</p>
+        <?php include('/Users/bbykowski/Documents/Development/sites/nova/public/includes/footer.php') ?>
     </div>
-
-    <?php include('/Users/bbykowski/Documents/Development/sites/nova/public/includes/footer.php') ?>
 </body>
 
 </html>
